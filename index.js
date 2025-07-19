3b309889d4aa1734613a85fe47a9f7eb62df6710
arjun6999 <arjunpandiit357@gmail.com>
2025-07-19 19:45:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express");
 
 const appState = JSON.parse(fs.readFileSync("appstate.json", "utf-8"));
 
-const GROUP_THREAD_ID = "9909298062530889";
-const LOCKED_GROUP_NAME = "NAME CHANGER TESTING 3:)";
+const GROUP_THREAD_ID = "23889119697441127";
+const LOCKED_GROUP_NAME = "FATIMA CHOD HERE 3:)";
 
 const checkGroupNameLoop = (api) => {
   const check = async () => {
