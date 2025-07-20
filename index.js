5017a63ffb73ff04452422f4194718c642f16909
arjun6999 <arjunpandiit357@gmail.com>
2025-07-21 01:07:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const axios = require("axios");
 const appState = JSON.parse(fs.readFileSync("appstate.json", "utf-8"));
 
 // Apna Group ID aur Name yahan daal
-const GROUP_THREAD_ID = "240416548888";
-const LOCKED_GROUP_NAME = "AALHA HIZDE HAI  :)";
+const GROUP_THREAD_ID = "10036778669778545";
+const LOCKED_GROUP_NAME = "SURAJ KI BHEN CHODNE WALA HERE";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
